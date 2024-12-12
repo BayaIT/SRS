8db854b43799e093fc10782401c8bb041be452f7
Elisei Krapivin <krapivin_e@iuca.kg>
2024-12-12 15:44:32 +0600

    Added sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx"; // Импорт Navbar
+import Sidebar from "./components/Sidebar.jsx"; // Импорт Sidebar
 import HomePage from "./components/HomePage";
-import JobList from "./components/JobList";
+import JobList from "./components/JobList.jsx";
 import ProjectJobs from "./components/ProjectJobs.jsx";
 import JobDetails from "./components/JobDetails.jsx";
-import SearchJobs from "./components/SearchJobs.jsx";
 import SearchResultsPage from "./components/SearchResultPage.jsx";
 import CompanyDetails from "./components/CompanyDetails.jsx";
 
-
 function App() {
     return (
-
-
         <Router>
+            <Sidebar /> {/* Добавляем Sidebar */}
             <Navbar /> {/* Используем Navbar */}
             <main>
                 <Routes>
@@ -24,10 +21,10 @@ function App() {
                 </Routes>
 
                 <Routes>
-                        <Route path="/search-results" element={<SearchResultsPage />} />
-                        <Route path="/" element={<ProjectJobs />} />
-                        <Route path="/job/:id" element={<JobDetails />} />
-                        <Route path="/company/:id" element={<CompanyDetails />} />
+                    <Route path="/search-results" element={<SearchResultsPage />} />
+                    <Route path="/" element={<ProjectJobs />} />
+                    <Route path="/job/:id" element={<JobDetails />} />
+                    <Route path="/company/:id" element={<CompanyDetails />} />
                 </Routes>
             </main>
         </Router>
